3ddbdd126759da17f5ae81a7ae6e3e077c4bdf3a
Fábio Kenji Matsuda <fabio@fkmatsuda.dev>
2022-01-24 13:17:17 -0300

    startdb no longer stops the database

--- postgres/startdb.sh
+++ postgres/startdb.sh
@@ -5,5 +5,4 @@ su postgres -c '/usr/lib/postgresql/13/bin/pg_ctl -D $PGDATA init' && \
 mv /pg_hba.conf $PGDATA/ && \
 chown postgres.postgres $PGDATA/pg_hba.conf && \
 su postgres -c '/usr/lib/postgresql/13/bin/pg_ctl -D $PGDATA start' && \    
-su postgres -c '/usr/lib/postgresql/13/bin/psql -d template1 -f /pginit.sql' && \
-su postgres -c '/usr/lib/postgresql/13/bin/pg_ctl -D $PGDATA stop'  
+su postgres -c '/usr/lib/postgresql/13/bin/psql -d template1 -f /pginit.sql'
